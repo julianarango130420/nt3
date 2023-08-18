4473feb079dfda91e6b53aedcdc4477cb89413c5
Luis Torres <luistorresco@gmail.com>
2023-08-18 10:15:23 -0500

    NT3 se realizo un control de temperatura y cantidad de alcohol

---
+++ condiciones.py
@@ -0,0 +1,8 @@
+temperatura=int(input("ingresa la temperatura"))
+cantAlcohol=int(input("ingresa cantida de alcohol"))
+
+if temperatura>= 50 or cantAlcohol>= 5:
+    print("Alarma")
+
+else:
+     print("Proceso con nirmalidad")
